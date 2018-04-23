87760146c4091da8628ac1ad7c0ae5d447639330
Felipe Oliveira Carvalho <felipekde@gmail.com>
2018-04-23 10:45:22 -0300

    Move Constructable and NonCopyable to test_classes.h

--- small_vector_test.cpp
+++ small_vector_test.cpp
@@ -14,116 +14,12 @@
 #include "array_ref.h"
 #include "small_vector.h"
 
+#include "test_classes.h"
+
 using foc::SmallVector;
 using foc::SmallVectorImpl;
-
-/// A helper class that counts the total number of constructor and
-/// destructor calls.
-class Constructable {
- public:
-  static int numConstructorCalls;
-  static int numMoveConstructorCalls;
-  static int numCopyConstructorCalls;
-  static int numDestructorCalls;
-  static int numAssignmentCalls;
-  static int numMoveAssignmentCalls;
-  static int numCopyAssignmentCalls;
-
- private:
-  bool constructed;
-  int value;
-
- public:
-  Constructable() : constructed(true), value(0) { ++numConstructorCalls; }
-
-  /*implicit*/ Constructable(int val) : constructed(true), value(val) { ++numConstructorCalls; }
-
-  /*implicit*/ Constructable(const Constructable &src) : constructed(true) {
-    value = src.value;
-    ++numConstructorCalls;
-    ++numCopyConstructorCalls;
-  }
-
-  /*implicit*/ Constructable(Constructable &&src) : constructed(true) {
-    value = src.value;
-    ++numConstructorCalls;
-    ++numMoveConstructorCalls;
-  }
-
-  ~Constructable() {
-    REQUIRE(constructed);
-    ++numDestructorCalls;
-    constructed = false;
-  }
-
-  Constructable &operator=(const Constructable &src) {
-    REQUIRE(constructed);
-    value = src.value;
-    ++numAssignmentCalls;
-    ++numCopyAssignmentCalls;
-    return *this;
-  }
-
-  Constructable &operator=(Constructable &&src) {
-    REQUIRE(constructed);
-    value = src.value;
-    ++numAssignmentCalls;
-    ++numMoveAssignmentCalls;
-    return *this;
-  }
-
-  int getValue() const { return abs(value); }
-
-  static void reset() {
-    numConstructorCalls = 0;
-    numMoveConstructorCalls = 0;
-    numCopyConstructorCalls = 0;
-    numDestructorCalls = 0;
-    numAssignmentCalls = 0;
-    numMoveAssignmentCalls = 0;
-    numCopyAssignmentCalls = 0;
-  }
-
-  static int getNumConstructorCalls() { return numConstructorCalls; }
-
-  static int getNumMoveConstructorCalls() { return numMoveConstructorCalls; }
-
-  static int getNumCopyConstructorCalls() { return numCopyConstructorCalls; }
-
-  static int getNumDestructorCalls() { return numDestructorCalls; }
-
-  static int getNumAssignmentCalls() { return numAssignmentCalls; }
-
-  static int getNumMoveAssignmentCalls() { return numMoveAssignmentCalls; }
-
-  static int getNumCopyAssignmentCalls() { return numCopyAssignmentCalls; }
-
-  friend bool operator==(const Constructable &c0, const Constructable &c1) {
-    return c0.getValue() == c1.getValue();
-  }
-
-  friend bool ATTRIBUTE_UNUSED operator!=(const Constructable &c0, const Constructable &c1) {
-    return c0.getValue() != c1.getValue();
-  }
-};
-
-int Constructable::numConstructorCalls;
-int Constructable::numCopyConstructorCalls;
-int Constructable::numMoveConstructorCalls;
-int Constructable::numDestructorCalls;
-int Constructable::numAssignmentCalls;
-int Constructable::numCopyAssignmentCalls;
-int Constructable::numMoveAssignmentCalls;
-
-struct NonCopyable {
-  NonCopyable() {}
-  NonCopyable(NonCopyable &&) {}
-  NonCopyable &operator=(NonCopyable &&) { return *this; }
-
- private:
-  NonCopyable(const NonCopyable &) = delete;
-  NonCopyable &operator=(const NonCopyable &) = delete;
-};
+using foc::Constructable;
+using foc::NonCopyable;
 
 ATTRIBUTE_USED void CompileTest() {
   SmallVector<NonCopyable, 0> v;
